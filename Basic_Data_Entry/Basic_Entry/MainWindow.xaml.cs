ff9d7b12e05efb7dfc41f92ff15e85261728ebdf
Norumai01 <johnnydinhnguyen2001@gmail.com>
2024-06-02 20:47:40 -0400

    Basic entry form layout.

---
+++ Basic_Data_Entry/Basic_Entry/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using OfficeOpenXml;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.IO;
+
+namespace Basic_Entry
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string ExcelFilePath = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            if (int.TryParse(AgeTextBox.Text, out int age))
+            {
+                MessageBox.Show("Data saved successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid age.");
+            }
+        }
+    }
+}
